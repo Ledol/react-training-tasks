46ee76235d164ec511820e12019f154d53ea96df
Dzmitry Liavonenka <dzmitryliavonenka@gmail.com>
2022-08-31 20:37:05 +0300

    feat: add select component. Create manual select with styles and add Select.stories

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,83 @@
+import {KeyboardEvent, useEffect, useState} from "react";
+import style from './Select.module.css'
+
+export type ItemType = {
+    title: string
+    value: any
+}
+
+type SelectPropsType = {
+    value?: any
+    items: ItemType[]
+    onClick: (value: any) => void
+}
+
+export const Select = (props: SelectPropsType) => {
+    const [active, setActive] = useState(false)
+    const [hoveredElementValue, setHoveredElementValue] = useState(props.value)
+
+
+    const selectedItem = props.items.find(i => i.value === props.value)
+    const hoveredItem = props.items.find(i => i.value === hoveredElementValue)
+
+    useEffect(() => {
+        setHoveredElementValue(props.value)
+    }, [props.value])
+
+
+    const toggleItem = () => setActive(!active)
+
+    const onItemClick = (value: any) => {
+        props.onClick(value);
+        toggleItem();
+    }
+    const onKeyUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElementValue) {
+                    const pretendentElement = e.key === "ArrowDown"
+                    ? props.items[i + 1]
+                        : props.items[i - 1]
+
+                    if (pretendentElement) {
+                        props.onClick(pretendentElement.value)
+                        return;
+                    }
+                }
+            }
+            if(!selectedItem){
+                props.onClick(props.items[0].value);
+            }
+
+        }
+
+        if (e.key === "Enter" || e.key === "Escape") {
+            setActive(false)
+        }
+    }
+
+    return (
+        <>
+            {/*<select>
+                <option value="">Minsk</option>
+                <option value="">Moscow</option>
+                <option value="">Kiev</option>
+            </select>*/}
+            <div className={style.select} onKeyUp={onKeyUpHandler} tabIndex={0}>
+                <span className={style.main} onClick={toggleItem}>{selectedItem && selectedItem.title}</span>
+                {active &&
+                    <div className={style.items}>
+                        {props.items.map(item => <div
+                            onMouseEnter={() => setHoveredElementValue(item.value)}
+                            className={style.item + " " + (hoveredItem === item ? style.selected : "")}
+                            key={item.value}
+                            onClick={() => onItemClick(item.value)}
+                        >{item.title}
+                        </div>)}
+                    </div>
+                }
+
+            </div>
+        </>
+    )
+}
